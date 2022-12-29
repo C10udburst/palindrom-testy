f29f85f0588e2386ebb9a0b95210be1f5702a6e6
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-12-29 15:23:23 +0100

    jak ktos nie chce valgrinda to czemu nie

--- porownaj.sh
+++ porownaj.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-gcc @opt main.c -o main
+echo -e "Odpalic z valgrind? \033[1m(w chuj wolno)\033[0m"
+echo -n "[T]ak/[N]ie "
+read yn
+case $yn in
+    T*|t*|Y*|y*|1 ) valgrind=1;;
+    * ) valgrind=0;;
+esac
+if [[ "$valgrind" -ne 0 ]] ; then
+    gcc @opt main.c -o main
+else
+    gcc @opt -fsanitize=leak main.c -o main
+fi
 i=0
+echo "Valgrind: $valgrind"
 for d in ./wyniki/*; do
     cd $d
     echo "Testowanie: $d"
-    cat in | valgrind --leak-check=full -q --error-exitcode=1 ../../main > out2
+    if [[ "$valgrind" -ne 0 ]] ; then
+        cat in | valgrind --leak-check=full -q --error-exitcode=1 ../../main > out2
+    else
+        cat in | ../../main > out2
+    fi
     if [[ "$?" -ne 0 ]] ; then
         echo -e "\033[0;31mBłąd wykonania\033[0m"
         if [ -f "name" ]; then
